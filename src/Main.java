95ea51675cc2086af3152408be2b8a5800b300f1
Notnik_kg <aliaskartemirbekov@gmail.com>
2024-01-10 12:08:45 +0300

    Temirbekov Aliaskar

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
         System.out.println("Hello Java-12");
         System.out.println("Hello world!");
         System.out.println("Нурмухаммед Медетов");
+        System.out.println("Temirbekov Aliaskar ");
+
 
     }
 }
