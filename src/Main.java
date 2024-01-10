87fb0915ef948291dd14e4d62c11066002006254
Jandar <xp3335@gmail.com>
2024-01-10 15:13:00 +0600

    dobavil fioooo taalaibekov jandar

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello Java-12");
         System.out.println("Hello world!");
         System.out.println("Нурмухаммед Медетов");
+        System.out.println("🔴🔴🔴🔴🔴🔴🔴🔴Таалайбеков Жандар\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
 
     }
