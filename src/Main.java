94ca9cf7da60a32f44c9a86b8c066d9190280cf2
Lala <@ss>
2024-01-10 15:12:59 -0600

    add my name (Элебесов Муктарбек)

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("Hello Java-12");
         System.out.println("Hello world!");
         System.out.println("Нурмухаммед Медетов");
-
+        System.out.println("Элебесов Муктарбек");
     }
 }
