b5be35e01a842f7a00d9a2674d611c89319bdf24
KandybekDev <kandybeki@gmail.com>
2024-01-10 15:17:09 +0600

    Leave message

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello Java-12");
         System.out.println("Hello world!");
         System.out.println("Нурмухаммед Медетов");
+        System.out.println("kandyneklava11");
 
     }
 }
