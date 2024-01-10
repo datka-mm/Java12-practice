6adfe38eab14ded6fe4ff6f51a410fe455295bbf
Gulum <gulumkanusonkyzy@gmail.com>
2024-01-10 15:08:54 +0600

    Deleted massege Gulumkan

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,5 @@ public class Main {
         System.out.println("Hello Java-12");
         System.out.println("Hello world!");
         System.out.println("Нурмухаммед Медетов");
-        System.out.println("Gulumkan Uson kyzy ");
-
     }
 }
