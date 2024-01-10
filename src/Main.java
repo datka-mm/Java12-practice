c2a135a14e311dc760694e97468914ea8bc7f5ec
Myrzaiym Keldibekova <myrzaiymkeldibekova1@gmail.com>
2024-01-10 15:57:40 +0600

    second

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Нурмухаммед Медетов");
         System.out.println("Keldibekova Myrzaiym");
+        System.out.println("Keldibekova Myrzaiym");
 
     }
 }
