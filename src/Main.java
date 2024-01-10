4035b7e4b3c5ad8644495554c5b09556124fae67
Lala <@ss>
2024-01-10 15:38:13 -0600

    add muktar

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,9 @@ public class Main {
         System.out.println("Hello Java-12");
         System.out.println("Hello world!");
         System.out.println("Нурмухаммед Медетов");
-<<<<<<< HEAD
-        System.out.println("Элебесов Муктарбек");
-=======
         System.out.println("Keldibekova Myrzaiym");
         System.out.println("Nurlan Abiibillaev");
         System.out.println("Nurislam Toygonbaev");
->>>>>>> 094af0a5f2142d2f738473612aca5550a566d80f
+        System.out.println("Элебесов Муктарбек");
     }
 }
